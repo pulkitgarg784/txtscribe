d8282671a3149e9fe7546aebfea25c6bb45234d8
Pulkit Garg <pulkitgarg784@gmail.com>
2022-09-17 16:01:23 -0400

    Photo to text working

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
-from PIL import Image
-from pytesseract import pytesseract
-pytesseract.tesseract_cmd = r'/opt/homebrew/Cellar/tesseract/5.2.0/bin/tesseract'
+def detect_document(path):
+    words = []
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient.from_service_account_json("HackTheNorth-4bd17a719544.json")
 
-print(pytesseract.image_to_string(Image.open('img.jpeg')))
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+    print(response)
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                print(paragraph.words)
+                for word in paragraph.words:
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                    words.append(word_text)
+    print('text:', " ".join(map(str, words)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+detect_document("photo.jpeg")
