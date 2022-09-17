0ccb63e90369596cabf5379a5c8ca4ef6b316143
Pulkit Garg <pulkitgarg784@gmail.com>
2022-09-17 16:27:33 -0400

    got cohere working

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import cohere
+co = cohere.Client('dG9FO5NCPecnNjWtiSGAlXmQf3qbkVkBGX2krkBg')
+
 def detect_document(path):
     words = []
     from google.cloud import vision
@@ -10,17 +13,15 @@ def detect_document(path):
     image = vision.Image(content=content)
 
     response = client.document_text_detection(image=image)
-    print(response)
     for page in response.full_text_annotation.pages:
         for block in page.blocks:
             for paragraph in block.paragraphs:
-                print(paragraph.words)
                 for word in paragraph.words:
                     word_text = ''.join([
                         symbol.text for symbol in word.symbols
                     ])
                     words.append(word_text)
-    print('text:', " ".join(map(str, words)))
+    return(" ".join(map(str, words)))
 
     if response.error.message:
         raise Exception(
@@ -28,4 +29,18 @@ def detect_document(path):
             'https://cloud.google.com/apis/design/errors'.format(
                 response.error.message))
 
-detect_document("photo.jpeg")
+test_str = detect_document("photo.jpeg")
+print(test_str)
+training_data = "This is a spell check generator that fixes samples of text.\n\nSample: a new type OF aurora FounD on saturn resolves a planetary mystery\nFixed: A new type of aurora found on saturn resolves a planetary mystery\n--\nSample: online Shopping is ReSHaping Real-world Cities\nFixed: Online shopping is reshaping real-world cities\n--\nSample: When you close 100 TAbs AFter Finding THE SoluTion To A BuG\nFixed: When you close 100 tabs after finding the solution to a bug\n--\nSample: masteing DYNAmIC ProGrammING\nFixed: Mastering dynamic programming\n--\nSample: "+test_str+"\nFixed:"
+response = co.generate(
+  model='large',
+  prompt=training_data,
+  max_tokens=50,
+  temperature=0.3,
+  k=0,
+  p=0.75,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop_sequences=["--"],
+  return_likelihoods='NONE')
+print('Prediction: {}'.format(response.generations[0].text))
